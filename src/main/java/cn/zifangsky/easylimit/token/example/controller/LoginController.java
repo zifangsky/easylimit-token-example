55b6b7002b8b704390dab85e565ba2ecdaa54345
zifangsky <admin@zifangsky.cn>
2020-05-31 16:37:16 +0800

    优化测试代码

--- src/main/java/cn/zifangsky/easylimit/token/example/controller/LoginController.java
+++ src/main/java/cn/zifangsky/easylimit/token/example/controller/LoginController.java
@@ -7,7 +7,6 @@ import cn.zifangsky.easylimit.authc.impl.UsernamePasswordValidatedInfo;
 import cn.zifangsky.easylimit.enums.EncryptionTypeEnums;
 import cn.zifangsky.easylimit.session.impl.support.SimpleAccessToken;
 import cn.zifangsky.easylimit.session.impl.support.SimpleRefreshToken;
-import cn.zifangsky.easylimit.token.example.Constants;
 import cn.zifangsky.easylimit.token.example.model.SysUser;
 import cn.zifangsky.easylimit.utils.SecurityUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Map;
@@ -70,20 +68,16 @@ public class LoginController {
                 access.login(validatedInfo);
             }
 
-            //2. session中添加用户信息
-            HttpSession session = request.getSession();
-            session.setAttribute(Constants.SESSION_USER, access.getPrincipalInfo().getPrincipal());
-
-            //3. 获取Access Token和Refresh Token
+            //2. 获取Access Token和Refresh Token
             SimpleAccessToken accessToken = access.getAccessToken();
             SimpleRefreshToken refreshToken = access.getRefreshToken();
 
-            //4. 返回给页面的数据
+            //3. 返回给页面的数据
             result.put("code",200);
             result.put("access_token", accessToken.getAccessToken());
             result.put("refresh_token", refreshToken.getRefreshToken());
             result.put("expires_in", accessToken.getExpiresIn());
-            result.put("user_info", accessToken.getPrincipalInfo().getPrincipal());
+//            result.put("user_info", accessToken.getPrincipalInfo().getPrincipal());
         }catch (Exception e){
             result.put("code", 500);
             result.put("msg", "登录失败，用户名或密码错误！");
@@ -142,22 +136,19 @@ public class LoginController {
     @ResponseBody
     public Map<String,Object> logout(HttpServletRequest request){
         Map<String,Object> result = new HashMap<>(1);
-        HttpSession session = request.getSession();
-        SysUser user = (SysUser) session.getAttribute(Constants.SESSION_USER);
+
+        Access access = SecurityUtils.getAccess();
+        SysUser user = (SysUser) access.getPrincipalInfo().getPrincipal();
 
         if(user != null){
             logger.debug(MessageFormat.format("用户[{0}]正在退出登录", user.getUsername()));
         }
 
         try {
-            //1. 移除session中的数据
-            session.removeAttribute(Constants.SESSION_USER);
-
-            //2. 退出登录
-            Access access = SecurityUtils.getAccess();
+            //1. 退出登录
             access.logout();
 
-            //3. 返回状态码
+            //2. 返回状态码
             result.put("code", 200);
         }catch (Exception e){
             result.put("code",500);
